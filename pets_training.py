ee1ef4d8e108e7b294bafaa6d7a12b65ec63bd55
saymont <saymonp.trevisan@gmail.com>
2020-02-13 20:31:43 -0300

    Treina e salva modelo e guarda resultados

--- pets_training.py
+++ pets_training.py
@@ -1,6 +1,8 @@
 import logging
 import pickle
 
+from datetime import datetime
+
 from tensorflow import keras, get_logger
 
 logger = get_logger()
@@ -37,3 +39,5 @@ model.fit(x, y, batch_size=16, epochs=7)
 
 scores = model.evaluate(x, y)
 print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1] * 100))
+
+model.save(f"{scores[1]*100}{datetime.utcnow().strftime('%Y%m%d%H%M%S')}_model.h5")
